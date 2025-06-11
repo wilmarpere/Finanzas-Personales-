17b3c76d348a566bb41251fbc60771450799fd2a
Brayam96 <165792204+Brayam96@users.noreply.github.com>
2025-06-11 07:45:11 -0500

    Creacion del jwt.ts y el criptoKey, y agregue 2 dependencias

--- Dependencias/dependencias.ts
+++
@@ -1,4 +0,0 @@
-export { Application , Router } from 'https://deno.land/x/oak@v17.1.3/mod.ts';
-export {Client} from "https://deno.land/x/mysql@v2.12.1/mod.ts";
-export { z } from "https://deno.land/x/zod@v3.24.1/mod.ts";
-export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
