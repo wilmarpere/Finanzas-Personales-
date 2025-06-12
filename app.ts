82758b9ed4edab1951e85b2b1a11e3c6ec54c308
Brayam96 <165792204+Brayam96@users.noreply.github.com>
2025-06-12 06:57:34 -0500

    crecacion de las rutas par averificar el token

--- app.ts
+++ app.ts
@@ -4,11 +4,12 @@ import { Application,oakCors } from "./Dependencies/dependencias.ts";
 import{UsuarioRutes} from "./Routes/usuarioRoutes.ts"
 import console from "node:console";
 import { LoginRouter } from "./Routes/LoginRoutes.ts";
+import { UserRouter } from "./Routes/userRou.ts";
 
 const app = new Application();
 app.use (oakCors());
 
-const routes =[UsuarioRutes,LoginRouter]
+const routes =[UsuarioRutes,LoginRouter,UserRouter]
 
 routes.forEach((route)=>{
     app.use(route.routes());
